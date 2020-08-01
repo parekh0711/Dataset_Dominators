454e0132b995a85ca3b29fb4db7d8218da38bee8
Soham Parekh <54930568+parekh0711@users.noreply.github.com>
2020-08-01 20:18:17 +0530

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -10,7 +10,7 @@ urbansound_basepath = '/Noise'
 
 mcv = MozillaCommonVoiceDataset(mozilla_basepath, val_dataset_size=10)
 clean_train_filenames, clean_val_filenames = mcv.get_train_val_filenames()
-us8K = UrbanSound8K(urbansound_basepath, val_dataset_size=200)
+us8K = UrbanSound8K(urbansound_basepath, val_dataset_size=1)
 noise_train_filenames, noise_val_filenames = us8K.get_train_val_filenames()
 
 windowLength = 256
@@ -18,17 +18,16 @@ config = {'windowLength': windowLength,
           'overlap': round(0.25 * windowLength),
           'fs': 16000,
           'audio_max_duration': 0.8}
-
+# print(len(noise_train_filenames),len(noise_val_filenames))
 val_dataset = Dataset(clean_val_filenames, noise_val_filenames, **config)
 val_dataset.create_tf_record(prefix='val', subset_size=1)
-
+# input("Done")
 train_dataset = Dataset(clean_train_filenames, noise_train_filenames, **config)
-train_dataset.create_tf_record(prefix='train', subset_size=1)
-
+train_dataset.create_tf_record(prefix='train', subset_size=9)
+input("done")
 ## Create Test Set
-clean_test_filenames = mcv.get_test_filenames()
-
-noise_test_filenames = us8K.get_test_filenames()
-
-test_dataset = Dataset(clean_test_filenames, noise_test_filenames, **config)
-test_dataset.create_tf_record(prefix='test', subset_size=1000, parallel=False)
+# clean_test_filenames = mcv.get_test_filenames()
+# noise_test_filenames = us8K.get_test_filenames()
+#
+# test_dataset = Dataset(clean_test_filenames, noise_test_filenames, **config)
+# test_dataset.create_tf_record(prefix='test', subset_size=100, parallel=False)
