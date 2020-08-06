016a2174136a55487e39411ff864ad2c7919c228
vasvi1203 <64128681+vasvi1203@users.noreply.github.com>
2020-08-07 00:49:55 +0530

    Changed subset size

--- create_dataset.py
+++ create_dataset.py
@@ -8,7 +8,7 @@ warnings.filterwarnings(action='ignore')
 mozilla_basepath = 'Speech-Dataset-in-Hindi-Language/'
 urbansound_basepath = 'Noise/'
 
-mcv = MozillaCommonVoiceDataset(mozilla_basepath, val_dataset_size=5)
+mcv = MozillaCommonVoiceDataset(mozilla_basepath, val_dataset_size=55)
 clean_train_filenames, clean_val_filenames = mcv.get_train_val_filenames()
 
 us8K = UrbanSound8K(urbansound_basepath, val_dataset_size=200)
@@ -21,10 +21,10 @@ config = {'windowLength': windowLength,
           'audio_max_duration': 0.8}
 
 val_dataset = Dataset(clean_val_filenames, noise_val_filenames, **config)
-val_dataset.create_tf_record(prefix='val', subset_size=1)
+val_dataset.create_tf_record(prefix='val', subset_size=11)
 
 train_dataset = Dataset(clean_train_filenames, noise_train_filenames, **config)
-train_dataset.create_tf_record(prefix='train', subset_size=50)
+train_dataset.create_tf_record(prefix='train', subset_size=800)
 
 ## Create Test Set
 '''clean_test_filenames = mcv.get_test_filenames()
